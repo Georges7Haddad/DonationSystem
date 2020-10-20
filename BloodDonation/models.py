ebd2682727b9b8e9f286a0375ff2bb11e2eab7ef
NaderAS <nha59@mail.aub.edu>
2020-10-20 12:45:18 +0300

    a

--- BloodDonation/models.py
+++ BloodDonation/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Donors(models.Model):
+    first_name = models.CharField(max_length=26)
+    last_name = models.CharField(max_length=26)
+    date_of_birth = models.DateField()
+    phone_number = models.IntegerField()
+    email_address = models.EmailField()
+    blood_type = models.CharField(max_length=5)
+    location = models.CharField(max_length=26)
+    last_time_donated = models.DateField()
+    can_donate = models.CharField(max_length=10)
+
---
+++ venvv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\smart pc\desktop\donationsystem\venvv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
