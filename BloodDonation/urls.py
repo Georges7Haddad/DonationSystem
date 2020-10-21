0fbad637627eccbc1e413d8a42b006b63bdfd859
Kazoud <ksdahrouge@gmail.com>
2020-10-21 05:19:59 +0300

    Added urls for request form, request confirmation and donor confirmation

--- BloodDonation/urls.py
+++ BloodDonation/urls.py
@@ -4,4 +4,7 @@ from BloodDonation import views
 
 urlpatterns = [
     path('', views.register_donor),
+    path('request/', views.register_request),
+    path('confirm/donor', views.confirmation_message_donor),
+    path('confirm/request', views.confirmation_message_request),
 ]
--- BloodDonation/views.py
+++ BloodDonation/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from BloodDonation.forms import DonorForm
 from BloodDonation.models import Donor
+from django.http import HttpResponse
 
 
 def register_donor(request):
@@ -13,3 +14,15 @@ def register_donor(request):
 
     donor_form = DonorForm()
     return render(request=request, template_name="register_donor.html", context={"donor_form": donor_form})
+
+
+def register_request(request):
+    return HttpResponse("This link will contain the request form.")
+
+
+def confirmation_message_donor(request):
+    return HttpResponse("Thank you for filling this form.")
+
+
+def confirmation_message_request(request):
+    return HttpResponse("Requests have been sent to people who are able to donate.")
